171a00ddf32ffc1405125e5fc6dad3723bf970b0
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:03:23 +0530

    string2

---
+++ string2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string a;
+	cin >> a;
+	
+	int i;
+	
+	for(i=0; a[i]!='\0'; i++)
+	{
+	}
+	
+	cout<< endl << "No of letters -> " << i;
+
+	return 0;
+}
