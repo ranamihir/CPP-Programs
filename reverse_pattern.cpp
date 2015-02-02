fb6806e9298425f9c1d29e0991d35d7dee5e8813
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:10:40 +0530

    reverse_pattern

---
+++ reverse_pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int n=0, i=0, j=0, k=0;
+	
+	cout << "Enter the number of rows: " << endl;
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i - 1; j++)
+		{
+			cout << " ";
+		}
+		
+		for (int k = 1; k <= 2*n + 1 - 2*i; k++)
+		{
+			cout << "*";
+		}
+		
+		cout << endl;
+	}
+	
+	return 0;
+}
