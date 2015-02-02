89543b095efe195f4facf44e96c4f3bb67327e35
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:14:10 +0530

    while

---
+++ while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	
+	int product = 3;
+	
+	while (product <= 100)
+	{
+		cout << product << endl;
+		product = product*3;
+	}
+	
+	
+	return 0;
+	
+}
