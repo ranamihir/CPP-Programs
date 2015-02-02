3b0676d7f349ad89777b910a20003a6cf6d593ba
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:14:03 +0530

    for

---
+++ for.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int x, sum = 0;
+	
+	for (int x = 2; x <= 20; x += 2)
+	
+	sum += x;
+	cout << "The sum is: " << sum << endl;
+	
+	return 0;
+	
+}
