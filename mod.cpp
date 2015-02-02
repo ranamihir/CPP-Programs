8c89af11ac757180cc07a11ad867bbd9ec7f3397
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:13:41 +0530

    mod

---
+++ mod.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+int x;
+cout << "enter the value of x: " << endl;
+cin >> x;
+
+int mod = fabs(x);
+
+cout << mod << endl;
+
+return 0;
+
+}
