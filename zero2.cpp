7c7c058f22890e870eecd45b2668dd2e7d2b965e
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:12:00 +0530

    zero2

---
+++ zero2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int zero(int x)
+{
+	int a;
+ 	int s=0;
+ 	
+ 	int k=1;
+ 	do {
+	a = x%10;
+	x = x/10;
+	
+	if(a!=0)
+	{
+		s+=k*a;
+		k*=10;
+	}
+	
+	} while (x != 0);
+	return s;
+}
+
+int main()
+{
+	int n=0;
+	
+	cout << "Enter the number: " << endl;
+	cin >> n;
+	
+	cout << "The modified number is: "<<zero(n);
+	
+	
+	return 0;
+}
+			
