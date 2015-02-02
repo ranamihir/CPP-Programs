78ef4b74c4ad6017b630e022de90dfc40147e25a
Mihir Rana <ranamihir@gmail.com>
2015-02-02 17:09:40 +0530

    test_cmath

---
+++ test_cmath.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+	float x = 10.6;
+	
+	cout << fabs(x) << endl;
+	cout << floor(x) << endl;
+	cout << ceil(x) << endl;
+	
+	return 0;	
+}
