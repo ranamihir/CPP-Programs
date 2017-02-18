f584e1131947581c02bf97b25b76fe431325f613
Mihir Rana <ranamihir@gmail.com>
2017-02-19 02:11:40 +0530

    Recursive and iterative binary search.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-bool binarySearch(int *a,int size,int b,int low1,int high1)
-{
-	int high = high1;
-	int low = low1;
-	int n = size;
-	int mid = (low+high)/2;
-	do
-	{
-		if(a[mid] ==b)
-		return true;
-		if(a[mid]<b)
-		{
-			low = mid+1;
-			mid = (low+high)/2;
-		}
-		if(a[mid]>b)
-		{
-			high = mid-1;
-			mid = (low+high)/2;
-		}
-	}while(low!=high && a[mid]!=b);
-	if(a[mid] == b)
-	return true;
-	return false;
-}
-int main()
-{
-	int n;
-	cout<<"enter the size of array"<<endl;
-	cin>>n;
-	int *a = new int[n];
-	cout<<"enter the element of array"<<endl;
-	for(int i = 0;i<n;i++)
-	{
-		cin>>a[i];
+int binarySearch_Iterative(const vector <int> &, int);
+int binarySearch_Recursive(const vector <int> &, int, int, int);
+int main(){
+	int n, value, i, x;
+	cin >> n;
+	vector <int> a;
+	for(i = 0; i < n; i++) {
+		cin >> value;
+		a.push_back(value);
 	}
-	cout<<"enter the element to be searched"<<endl;
-	int b;
-	cin>>b;
-	if(binarySearch(a,n,b,0,n-1))
-	cout<<"the element exists"<<endl;
-	else
-	cout<<"it doesnt exists"<<endl;
+	sort(a.begin(), a.end());
+	cin >> x;
+	cout << binarySearch_Iterative(a, x) << endl;
+	cout << binarySearch_Recursive(a, x, 0, a.size() - 1) << endl;
+}
+int binarySearch_Iterative(const vector <int> &a, int x) {
+	int low = 0, high = a.size() - 1, mid;
+	while(low <= high) {
+		mid = low + (high-low)/2;
+		if (a[mid] == x)
+			return true;
+		else if (a[mid] < x)
+			low = mid+1;
+		else
+			high = mid-1;
+	}
+	return -1;
+}
+int binarySearch_Recursive(const vector <int> &a, int x, int low, int high) {
+	while(low <= high) {
+		int mid = low + (high-low)/2;
+		if (a[mid] == x)
+			return true;
+		else if (a[mid] < x)
+			return binarySearch_Recursive(a, x, mid+1, high);
+		else
+			return binarySearch_Recursive(a, x, low, mid-1);
+	}
+	return -1;
 }
